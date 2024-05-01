d3d822b5dce6a55912fd74da22c1fc4046951ed3
maxymlyskov <raypember02@gmail.com>
2024-05-01 23:16:50 +0300

    Add login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { UserResponseInteface } from './types/user-response.interface';
 import { UsersService } from './users.service';
 
@@ -11,6 +12,13 @@ export class UsersController {
     @UsePipes(new ValidationPipe())
     async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInteface> {
         const user = await this.usersService.createUser(createUserDto);
+        return this.usersService.buildUserResponse(user);
+    }
+
+    @Post('login')
+    @UsePipes(new ValidationPipe())
+    async login(@Body('user') loginUserDto: LoginUserDto): Promise<UserResponseInteface> {
+        const user = await this.usersService.login(loginUserDto);
         return this.usersService.buildUserResponse(user);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { compare } from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseInteface } from './types/user-response.interface';
 import { UserEntity } from './users.entity';
-
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(UserEntity) private readonly usersRepository: Repository<UserEntity>) { }
@@ -30,6 +30,30 @@ export class UsersService {
         Object.assign(newUser, createUserDto);
 
         return await this.usersRepository.save(newUser);
+    }
+
+    async login(loginUserDto: { email: string, password: string }): Promise<UserEntity> {
+        const user = await this.usersRepository.findOne({
+            where: {
+                email: loginUserDto.email,
+            },
+            select: ['id', 'username', 'bio', 'image', 'email', 'password']
+        });
+
+        if (!user) {
+            throw new HttpException('Credentails are wrong.', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        const isPasswordCorrect = await compare(loginUserDto.password, user.password);
+
+
+        if (!isPasswordCorrect) {
+            throw new HttpException('Credentails are wrong.', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        delete user.password
+
+        return user;
     }
 
     buildUserResponse(user: UserEntity): UserResponseInteface {
