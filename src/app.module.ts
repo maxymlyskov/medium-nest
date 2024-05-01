378bf4e79d1c52009ad09dc506f06d9a7692d30c
maxymlyskov <raypember02@gmail.com>
2024-05-01 19:53:40 +0300

    Add migrations config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import ormConfig from '@app/ormconfig';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), TagsModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    ConfigModule.forRoot(),
+    TagsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
