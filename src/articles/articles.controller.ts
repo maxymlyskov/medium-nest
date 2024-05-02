abbd8559024142afdbcedb881bc34185d9677ca3
maxymlyskov <raypember02@gmail.com>
2024-05-03 00:00:15 +0300

    Add types for article response.

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -4,6 +4,7 @@ import { UserEntity } from '@app/users/users.entity';
 import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
+import { ArticleResponseInterface } from './types/article-response.interface';
 
 @Controller('articles')
 export class ArticlesController {
@@ -12,7 +13,8 @@ export class ArticlesController {
     @Post()
     @UseGuards(AuthGuard)
     @UsePipes(new ValidationPipe())
-    async createArticle(@User() user: UserEntity, @Body('article') createArticleDto: CreateArticleDto): Promise<any> {
-        return await this.articlesService.createArticle(user, createArticleDto);
+    async createArticle(@User() user: UserEntity, @Body('article') createArticleDto: CreateArticleDto): Promise<ArticleResponseInterface> {
+        const article = await this.articlesService.createArticle(user, createArticleDto);
+        return this.articlesService.buildArticleResponse(article);
     }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -4,12 +4,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ArticleEntity } from './articles.entity';
 import { CreateArticleDto } from './dto/create-article.dto';
+import { ArticleResponseInterface } from './types/article-response.interface';
 
 @Injectable()
 export class ArticlesService {
     constructor(@InjectRepository(ArticleEntity) private readonly articleRepository: Repository<ArticleEntity>) { }
 
-    async createArticle(user: UserEntity, createArticleDto: CreateArticleDto): Promise<any> {
+    async createArticle(user: UserEntity, createArticleDto: CreateArticleDto): Promise<ArticleEntity> {
         const newArticle = new ArticleEntity();
         Object.assign(newArticle, createArticleDto);
 
@@ -21,5 +22,9 @@ export class ArticlesService {
         newArticle.slug = newArticle.title.toLowerCase().replace(/ /g, '-');
 
         return await this.articleRepository.save(newArticle)
+    }
+
+    buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+        return { article }
     }
 }
